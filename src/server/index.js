9f189a762eee975686aab8205f86f2bd1faf0ac1
Lethe <2963923+lethevimlet@users.noreply.github.com>
2022-10-17 13:58:11 +0000

    WIP

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
 const endpoint = require("./api/endpoint");
+const server = require("http").createServer(app)
+const io = require("socket.io")(server);
 
 app.use(express.static("src/webapp"));
 app.use(endpoint);
@@ -10,4 +12,8 @@ app.get("/test", function(req, res) {
   res.send("This is a test!");
 });
 
-app.listen(3000);
+io.on("connection", (socket) => {
+  console.log("WS -> User connected id: " + socket.id);
+});
+
+server.listen(3000);
