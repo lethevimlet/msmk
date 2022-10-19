a1a2380a530497ceafb2d70210efe2beff3e1a66
Lethe <2963923+lethevimlet@users.noreply.github.com>
2022-10-19 19:30:08 +0000

    WIP

---
+++ src/server/api/chat-endpoint.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const router = express.Router();
+
+var messages = ["hola", "test"];
+
+router.get("/chat/test", function(req, res, next) {
+  res.send("test chat");
+});
+
+router.get("/chat/list", function(req, res, next) {
+  var s = "";
+  messages.forEach(function(element){
+    s += element + "<br><br>"; 
+  });
+  res.send(s);
+});
+
+router.post("/chat/message", function(req, res, next) {
+  var body = req.body;
+  messages.push(body.message);
+  res.send();
+});
+
+
+module.exports = router;
--- src/server/index.js
+++ src/server/index.js
@@ -5,11 +5,13 @@ const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.use(express.static("src/webapp"));
+app.use(express.json());
 app.use(endpoint);
+app.use(require("./api/chat-endpoint"));
 
 
 app.get("/test", function(req, res) {
-  res.send("This is a test!");
+  res.redirect("https://google.com");
 });
 
 io.on("connection", (socket) => {
